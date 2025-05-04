3ea7ff7600fe87e159bd1deb71a3bef173e06c3c
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-04 20:47:16 +0300

    Add toggle mode component

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/contants';
+import ModeToggle from './ModeToggle';
 
 export default function Header () {
     return (
@@ -23,6 +24,7 @@ export default function Header () {
                     </Link>
                 </div>
                 <div className="space-x-2">
+                    <ModeToggle />
                     <Button asChild variant='ghost'>
                         <Link href='/cart'>
                             <ShoppingCart /> Cart
