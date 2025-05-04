4fa9ebf38f7a8ec1b91ff0cee01a58bff4c2adc2
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-04 19:03:11 +0300

    Install next-theme and use it . Add dropdown from shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import '@/assets/styles/globals.css'
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/contants";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({subsets: ['latin']})
-
+ 
 export const metadata: Metadata = {
   title: {
     template: `%s | Drip Cart`,
@@ -20,11 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning >
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+        >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
