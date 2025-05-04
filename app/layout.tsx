536854419f41b37818355f6a44652f5a704324d2
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-04 18:03:23 +0300

    Add root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import '@/assets/styles/globals.css'
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/contants";
 
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: "Drip Cart",
-  description: "Modern e-commerce build with NextJS",
+  title: {
+    template: `%s | Drip Cart`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
