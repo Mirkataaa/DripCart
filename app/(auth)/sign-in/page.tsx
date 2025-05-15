c77375a57e1475ed1ec4d584c27baa6a60efa2f0
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-15 23:17:46 +0300

    Hook up sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,15 +1,30 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { APP_NAME } from "@/lib/contants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import CredentialsSignInForm from "./CredentialsSignInForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  // Get session (server comp way)
+  const session = await auth();
+
+  if (session) {
+    return redirect("/");
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
@@ -24,10 +39,12 @@ export default function SignInPage() {
             />
           </Link>
           <CardTitle className="text-center">Sign In</CardTitle>
-          <CardDescription className="text-center">Sign In to your account</CardDescription>
+          <CardDescription className="text-center">
+            Sign In to your account
+          </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-            <CredentialsSignInForm />
+          <CredentialsSignInForm />
         </CardContent>
       </Card>
     </div>
