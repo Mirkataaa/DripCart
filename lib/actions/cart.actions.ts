355e7cef5c2856dae95986651c7d2c38ca93bc2a
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-18 11:39:15 +0300

    Price cal and add to database

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,10 +2,29 @@
 
 import { cookies } from "next/headers";
 import { CartItem } from "@/types";
-import { convertToPlainObject, formatError } from "../utils";
+import { convertToPlainObject, formatError, round2 } from "../utils";
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
-import { cartItemSchema } from "../validators";
+import { cartItemSchema, insertCartSchema } from "../validators";
+import { revalidatePath } from "next/cache";
+
+// Calculate cart prices
+
+function calcPrice(items: CartItem[]) {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+}
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -18,7 +37,6 @@ export async function addItemToCart(data: CartItem) {
     const session = await auth();
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
-    
     // Get cart
 
     const cart = await getMyCart();
@@ -30,21 +48,35 @@ export async function addItemToCart(data: CartItem) {
     // Find product in db
 
     const product = await prisma.product.findFirst({
-      where: {id: item.productId}
+      where: { id: item.productId },
     });
 
-    // TESTING
-    console.log({
-      "Session Cart ID": sessionCartId,
-      "User ID": userId,
-      'Item Requested': item,
-      'Product found': product,
-    });
+    if (!product) throw new Error("Product not found");
 
-    return {
-      success: true,
-      message: "Item add to cart",
-    };
+    if (!cart) {
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // Revalidate product page
+
+      revalidatePath(`/product/${product.slug}`);
+
+      return {
+        success: true,
+        message: "Item add to cart",
+      };
+    } else {
+      // Handle qty and multiple products
+    }
   } catch (error) {
     return {
       success: false,
@@ -54,29 +86,28 @@ export async function addItemToCart(data: CartItem) {
 }
 
 export async function getMyCart() {
-      const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
 
-    if (!sessionCartId) throw new Error("Cart session not found");
+  if (!sessionCartId) throw new Error("Cart session not found");
 
-    // Get session and user ID
-    const session = await auth();
-    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
-    // Get user cart from db 
-    const cart = await prisma.cart.findFirst({
-      where: userId ? {userId: userId} : {sessionCartId: sessionCartId}
-    });  
+  // Get user cart from db
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
 
-    if(!cart) return undefined;
+  if (!cart) return undefined;
 
-    // Convert decimals and return
-    return convertToPlainObject({
-      ...cart,
-      items: cart.items as CartItem[],
-      itemsPrice: cart.itemsPrice.toString(),
-      totalPrice: cart.totalPrice.toString(),
-      shippingPrice: cart.shippingPrice.toString(),
-      taxPrice: cart.taxPrice.toString(),
-    })
-    
+  // Convert decimals and return
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
