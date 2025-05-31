a881cb8da2347b65ebfaed28910db2152899c577
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-05-31 12:32:11 +0300

    Add buttons for add and remove item

--- app/(root)/cart/CartTable.tsx
+++ app/(root)/cart/CartTable.tsx
@@ -1,52 +1,131 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { useTransition } from "react";
-import { Cart } from "@/types";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import { Cart, CartItem, CartResponse } from "@/types";
 import Link from "next/link";
 import Image from "next/image";
-import {Table , TableBody , TableHead , TableHeader , TableRow , TableCell} from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
+export default function CartTable({ cart }: { cart?: Cart }) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-export default function CartTable ({cart} : {cart? : Cart}) {
+  const handleCartAction = (res: CartResponse) => {
+    if (!res.success) {
+      toast.error(res.message as string);
+      return;
+    }
 
-    const router = useRouter();
-    const [isPending , startTransition] = useTransition();
+    toast.custom((t) => (
+      <div
+        className={`${
+          t.visible ? "animate-enter" : "animate-leave"
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <Button
+          variant="ghost"
+          onClick={() => router.push("/cart")}
+          className="w-full"
+        >
+          {res.message}
+        </Button>
+      </div>
+    ));
+  };
 
-    return (
-        <>
-        <h1 className="py-4 h2-bold">Shopping Cart</h1>
-        {!cart || cart?.items.length === 0 ? (
-            <div>
-                Cart is empty. <Link href="/">Go Shopping</Link>
-            </div>
-        ) : (
-            <div className="grid md:grid-cols-4 md:gap-5 ">
-                <div className="overflow-x-auto md:col-span-3">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Item</TableHead>
-                                <TableHead className="text-center">Quantity</TableHead>
-                                <TableHead className="text-right">Price</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {cart.items.map((item) => (
-                                <TableRow key={item.slug}>
-                                    <TableCell>
-                                        <Link href={`/product/${item.slug}`} className="flex items-center">
-                                            <Image src={item.image} alt={item.name} width={50} height={50} />
-                                            <span className="px-2">{item.name}</span>
-                                        </Link>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-        )}
-        </>
-    );
+  const handleAddToCart = async (item: CartItem) => {
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      handleCartAction(res);
+    });
+  };
+
+  const handleRemoveFromCart = async (item: CartItem) => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+      handleCartAction(res);
+    });
+  };
+
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart?.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5 ">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        type="button"
+                        variant="outline"
+                        onClick={() => handleRemoveFromCart(item)}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="h-4 w-4 " />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        type="button"
+                        variant="outline"
+                        onClick={() => handleAddToCart(item)}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="h-4 w-4 " />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
