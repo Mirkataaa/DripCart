4b359d2d9b275e4b37c939fa5d58af786f2b3f18
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-06-01 14:51:00 +0300

    Create shipping address page , get user by id action & shipping schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -69,3 +69,17 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// Get use by id 
+
+export async function getUserById(userId:string) {
+  const user = await prisma.user.findFirst({
+    where: {id : userId}
+  });
+
+  if(!user) {
+    throw new Error('User not found')
+  };
+
+  return user;
+}
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { insertProductSchema , insertCartSchema , cartItemSchema, cartResponseSchema } from "@/lib/validators";
+import { insertProductSchema , insertCartSchema , cartItemSchema, cartResponseSchema , shippingAddressSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
@@ -10,3 +10,4 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type CartResponse = z.infer<typeof cartResponseSchema>
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>
