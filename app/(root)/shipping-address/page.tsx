15ce0fce2af34e5c1ef49ee45859b09ef0fbd83d
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-06-02 17:40:50 +0300

    Add checkoutSteps component

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,26 +5,31 @@ import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
 import ShippingAddressForm from "./ShippingAddressForm";
+import CheckoutSteps from "@/components/shared/CheckoutSteps";
 
 export const metadata: Metadata = {
-    title: 'Shipping Address'
-}
+  title: "Shipping Address",
+};
 
 export default async function ShippingAddressPage() {
+  const cart = await getMyCart();
 
-    const cart = await getMyCart();
+  if (!cart || cart.items.length === 0) redirect("/cart");
 
-    if(!cart || cart.items.length === 0) redirect('/cart');
+  const session = await auth();
 
-    const session = await auth();
+  const userId = session?.user?.id;
 
-    const userId = session?.user?.id;
+  if (!userId) throw new Error("No user ID");
 
-    if(!userId) throw new Error('No user ID');
-
-    const user = await getUserById(userId);
+  const user = await getUserById(userId);
 
   return (
-    <ShippingAddressForm address={user.address as ShippingAddress} ></ShippingAddressForm>
+    <>
+    <CheckoutSteps current={1}/>
+    <ShippingAddressForm
+      address={user.address as ShippingAddress}
+    ></ShippingAddressForm>
+    </>
   );
 }
